1443405e153f8aa48fd1a35187306acdb386f0ba
rh13 <rhlikhon13@gmail.com>
2018-04-04 23:14:17 +0600

    first five customers

---
+++ 05_get_five_customers.py
@@ -0,0 +1,30 @@
+'''
+This is a template code that you will use in this exercise.
+You only can change the value of query.
+
+'''
+
+# import sqlite
+import sqlite3
+
+# create database connection
+db = sqlite3.connect('employee_basic_crud.db')
+cursor = db.cursor()
+
+# write your query here
+query = ''' SELECT firstname, lastname, email
+                FROM customers LIMIT 5'''
+
+
+try:
+    cursor.execute(query)
+    all_rows = cursor.fetchall()
+    for row in all_rows:
+        print(row)
+except:
+    print('Something wrong in your query')
+
+# close database connection
+db.close()
+
+
